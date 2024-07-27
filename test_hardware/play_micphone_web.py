7931ab74c99188ed6b15ad2f976e1108486bb80f
mbk <zoucongyu1993@hotmail.com>
2024-07-27 16:42:07 +0200

    debug

--- test_hardware/play_micphone_web.py
+++ test_hardware/play_micphone_web.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template, request, jsonify
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from flask import Flask, render_template, jsonify
 import time
-import wave
 import pyaudio
 import threading
 import queue
@@ -21,6 +17,7 @@ is_recording = threading.Event()
 stream = None
 p = pyaudio.PyAudio()
 recording_thread = None
+
 
 def start_recording():
     global frames, stream
@@ -42,10 +39,12 @@ def start_recording():
     stream.stop_stream()
     stream.close()
 
+
 def stop_recording():
     is_recording.clear()
     if recording_thread is not None:
         recording_thread.join()
+
 
 def callback(in_data, frame_count, time_info, status):
     if is_recording.is_set():
@@ -54,9 +53,11 @@ def callback(in_data, frame_count, time_info, status):
         frames.put(in_data)
     return (in_data, pyaudio.paContinue)
 
+
 @app.route('/')
 def index():
     return render_template('index_web.html')
+
 
 @app.route('/start_recording', methods=['POST'])
 def start():
@@ -65,15 +66,18 @@ def start():
     recording_thread.start()
     return jsonify({"status": "recording started"})
 
+
 @app.route('/stop_recording', methods=['POST'])
 def stop():
     stop_recording()
     return jsonify({"status": "recording stopped"})
 
+
 @app.route('/play_audio', methods=['POST'])
 def play_audio():
     play_audio_from_frames(list(frames.queue), FORMAT, CHANNELS, RATE)
     return jsonify({"status": "audio played"})
+
 
 def play_audio_from_frames(frames, format, channels, rate):
     p = pyaudio.PyAudio()
@@ -89,6 +93,7 @@ def play_audio_from_frames(frames, format, channels, rate):
     stream.stop_stream()
     stream.close()
     p.terminate()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
