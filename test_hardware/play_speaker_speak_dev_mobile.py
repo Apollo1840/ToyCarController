5b6891b40f67b1ce3c26903c72e729699c496a44
mbk <zoucongyu1993@hotmail.com>
2024-08-02 14:27:18 +0200

    for mobile device

---
+++ test_hardware/play_speaker_speak_dev_mobile.py
@@ -0,0 +1,142 @@
+from flask import Flask, render_template, request, jsonify
+import time
+from collections import deque
+import pyaudio
+import ffmpeg
+import threading
+import logging
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Define parameters
+CHUNK = 1024
+FORMAT = pyaudio.paInt16  # Sampling format
+CHANNELS = 1  # Mono
+RATE = 48000  # Sampling rate
+frame_queue = deque(maxlen=100)  # Adjust maxlen as needed
+
+is_recording = threading.Event()
+stream = None
+p = pyaudio.PyAudio()
+recording_thread = None
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start_recording():
+    # record from client and speak
+    is_recording.set()
+    logger.info("Recording started at %s", datetime.now())
+    # time.sleep(10)
+    while is_recording.is_set():
+        if len(frame_queue) == 0:
+            time.sleep(0.1)
+        else:
+            logger.info(f"current queue size: {len(frame_queue)}")
+            logger.info("speaking at %s", datetime.now())
+            speak(frame_queue.popleft())
+
+    logger.info("Recording stopped at %s", datetime.now())
+
+
+def speak(wav_binary):
+    # Use FFmpeg to decode the binary data of the webm file to raw PCM data
+    process = (
+        ffmpeg
+            .input('pipe:0', format='webm')
+            .output('pipe:', format='wav')
+            .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
+    )
+
+    # Feed the binary data to ffmpeg's stdin
+    process.stdin.write(wav_binary)
+    process.stdin.close()
+
+    # Read the first few bytes of the WAV header to determine the sample rate and number of channels
+    # wav_header = process.stdout.read(44)  # WAV header is 44 bytes
+    # channels = int.from_bytes(wav_header[22:24], byteorder='little')
+    # sample_rate = int.from_bytes(wav_header[24:28], byteorder='little')
+
+    # Initialize PyAudio
+    p = pyaudio.PyAudio()
+
+    # Open a stream with PyAudio for playback
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    # output_device_index=6,
+                    output=True)
+
+    while is_recording.is_set():
+        data = process.stdout.read(CHUNK)
+        if not data:
+            logger.info("no stream at %s", datetime.now())
+            break
+        # logger.info("streaming at %s", datetime.now())
+        stream.write(data)
+
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Print or log stderr output
+    # stderr_output = process.stderr.read().decode('utf-8')
+    # logger.error(stderr_output)
+
+    # process.stderr.close()
+    process.stdout.close()
+    process.wait()
+
+    logger.info("speak ends at %s", datetime.now())
+
+
+@app.route('/')
+def index():
+    return render_template('index_speak_dev_mobile.html')
+
+
+# Initialize a counter (could be stored in a database or a more persistent way)
+upload_counter = 0
+
+
+@app.route('/upload_audio', methods=['POST'])
+def upload_audio():
+    global upload_counter
+
+    # Save the uploaded file with a unique name using the counter
+    # Increment the counter for each new upload
+    audio_data = request.files['audio_data']
+    # upload_counter += 1
+    # file_path = f'tmp/uploaded_audio_{upload_counter}.webm'
+    # audio_data.save(file_path)  # Save to a file for manual inspection
+    # with open(file_path, 'wb') as f:
+    #    f.write(audio_data.read())
+    # Optionally, you can log the file path
+    # logger.info(f"Saved audio file: {file_path}")
+
+    # Process the file (e.g., append to the queue for playback)
+    frame_queue.append(audio_data.read())
+
+    return "sound recorded"
+
+
+@app.route('/speak', methods=['POST'])
+def listen():
+    logger.info("speak button clicked at %s", datetime.now())
+    global recording_thread
+    if not is_recording.is_set():
+        recording_thread = threading.Thread(target=start_recording)
+        recording_thread.start()
+    else:
+        is_recording.clear()
+        if recording_thread is not None:
+            recording_thread.join()
+    return jsonify({"status": "speak toggled"})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'), port=5000)
